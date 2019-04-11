00dcb6c35045bdafb1289d569d40ad20b10de7c0
Jin Lim <limjinsun@gmail.com>
2019-04-11 20:14:38 +0100

    make http request to external api server

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,10 +15,14 @@
  */
 package com.example.android.quakereport;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
@@ -29,16 +33,44 @@ public class EarthquakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_list_view);
 
-        List<Earthquake> earthquakes = new JsonToListObject().getList();
+//        List<Earthquake> earthquakes = new JsonToListObject().getList();
 
-        EarthquakeAdapter earthquakeAdapter = new EarthquakeAdapter(this,
-                R.id.earthquake_single_item_layout, earthquakes);
-
-        ListView earthquakeListView = (ListView) findViewById(R.id.earthquake_list_view);
-        // findViewById() will look for view by ID.
-        // inside of This activity's context view = R.layout.earthquake_list_view
-        earthquakeListView.setAdapter(earthquakeAdapter);
+        String str = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=5&limit=10";
+        new EarthquakeTasks().execute(str);
 
     }
 
+
+    private class EarthquakeTasks extends AsyncTask <String, Void, List<Earthquake>> {
+
+        @Override
+        protected List<Earthquake> doInBackground(String... strings) {
+            // make http call
+            URL url = HttpUtill.createUrl(strings[0]);
+            String response = "";
+
+            try {
+                response = HttpUtill.makeHttpRequest(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return JsonToListObject.getList(response);
+        }
+
+        @Override
+        protected void onPostExecute(List<Earthquake> earthquakes) {
+            // update Adapter and UI
+            super.onPostExecute(earthquakes);
+
+            EarthquakeAdapter earthquakeAdapter = new EarthquakeAdapter(getApplicationContext(),
+                    R.id.earthquake_single_item_layout, earthquakes);
+
+            ListView earthquakeListView = (ListView) findViewById(R.id.earthquake_list_view);
+            // findViewById() will look for view by ID.
+            // inside of This activity's context view = R.layout.earthquake_list_view
+            earthquakeListView.setAdapter(earthquakeAdapter);
+
+        }
+    }
 }
